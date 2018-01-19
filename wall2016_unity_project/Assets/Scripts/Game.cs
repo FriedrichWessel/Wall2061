926f3e848c5a31aeb715058ad5e33e6e94baad86
dwarfenhoschi <dwarfenhoschi@gmail.com>
2018-01-19 15:03:31 +0100

    Add files via upload
    Added current State of Unity Project

---
+++ wall2016_unity_project/Assets/Scripts/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public List<Spawner> Spawners = new List<Spawner>();
+    public float StartFrequency = 5f;
+    public float FrequencyDecrease = 0.1f;
+    public GameObject GoodBlockObject;
+    public GameObject BadBlockObject;
+
+    public float ChanceForBadBlock = 0.1f;
+
+    private float _currentFrequency = 5f;
+    private float _spawnTimer = 0;
+    private bool _gameRunning = false; 
+
+	// Use this for initialization
+	void Start () {
+        _currentFrequency = StartFrequency;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_gameRunning)
+        {
+            if (_spawnTimer >= _currentFrequency)
+            {
+                _spawnTimer = 0;
+                int RandomSpawnerNumber = (int) Math.Floor((double)UnityEngine.Random.Range(0, Spawners.Count));
+                float RandomBlock = UnityEngine.Random.value;
+                GameObject ObjectToSpawn = GoodBlockObject;
+                if (RandomBlock <= ChanceForBadBlock)
+                {
+                    ObjectToSpawn = BadBlockObject;
+                }
+
+                Spawners[RandomSpawnerNumber].SpawnObject(ObjectToSpawn);
+                _currentFrequency = Mathf.Max(1, _currentFrequency - FrequencyDecrease); 
+            }else
+            {
+                _spawnTimer += Time.deltaTime;
+            }
+        }
+	}
+
+    public void StartGame()
+    {
+        _gameRunning = true;
+    }
+}
---
+++ wall2016_unity_project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnObject(GameObject prefab)
+    {
+        GameObject newObject = GameObject.Instantiate(prefab, this.transform, false);
+        newObject.transform.localPosition = new Vector3(0, 0, 0);
+    }
+}

45ea4b4181b8aab56c7e178c094fbb83e7b2059d
Kevin Blank <kevin@happy-tuesday.com>
2018-03-28 15:53:06 +0200

    Added more Information. Added stuff to fail screen.

--- wall2016_unity_project/Assets/Scripts/UserInterfaces/InformationScreenUI.cs
+++ wall2016_unity_project/Assets/Scripts/UserInterfaces/InformationScreenUI.cs
@@ -11,9 +11,9 @@ public class InformationScreenUI : GUIBase {
     private Dictionary<string, string> InfoDatabase = new Dictionary<string, string>()
     {
         {"CripplewareInfo", "Project Entry: Crippleware\nGoal: Control dissident citizens effectively\n\nIn Charge: Dr. Gabriela Volkov\n\nOverview: Dr. Volkov has created an implant that controls neurological pathways and makes subjects more suspectible to questioning and authority\n\nCurrent Status: First Testrun ongoing, awaiting results" },
-        {"CommissarInfo", "Not here yet" },
-        {"VolkovInfo", "Not here yet" },
-        {"AgentMcKenzie", "Not here yet" },
+        {"CommissarInfo", "Name: Stefan Bechtel\nOccupation: KoSi Commissar\n\nPersonality Analysis: Obedient, Quiet, Violent\n\nCharges: Multiple Murder - removed\nCurrent Status: Patroling in Kreuzberg\n\n---------------------------\nName: Avril Wrike\nOccupation: KoSi Commisar\n\nPersonality Analysis: Sadistic, Loyal, Manipulative\n\nCharges: None\n\nCurrent Status: Patroling in Kreuzberg" },
+        {"VolkovInfo", "Name: Dr. Gabriela Volkov\nOccupation: KoSi Scientist\n\nPersonality Analysis: Sadistic, Manipulative, Intelligent\n\nCharges: Illegal Experiments - removed\n\nCurrent Status: Working in KoSi Lab X56" },
+        {"AgentMcKenzie", "Name: Agent McKenzie\n\nOccupation: KoSi Obserer at Checkpoint Alpha\n\nPersoanlity Analysis: Focused, Hardliner, Stoic\n\nNotes: Family is marked for \"Safekeeping\" in case of crisis" },
     };
 
     public Text Informationtext = null;
